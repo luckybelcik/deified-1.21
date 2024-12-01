471e10c7a195172636436355c19473378e2a94a8
luckybelcik <luckybelcik@gmail.com>
2024-12-01 22:34:25 +0100

    Initial Commit

---
+++ src/main/java/com/luckybelcik/deified/Deified.java
@@ -0,0 +1,16 @@
+package com.luckybelcik.deified;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Deified implements ModInitializer {
+	public static final String MOD_ID = "deified";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/luckybelcik/deified/DeifiedClient.java
@@ -0,0 +1,10 @@
+package com.luckybelcik.deified;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeifiedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
